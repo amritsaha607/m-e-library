3a7ae7a2c9715969ced9d9c79daaf47b1fc00705
Amrit Saha <amritsaha607@gmail.com>
2025-12-02 15:44:28 +0530

    Add login URL to interaction app

--- interactionApp/urls.py
+++ interactionApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('friend-req', views.request_friendship, name='friend_request'),
     path('follow', views.follow_user, name='follow'),
+    path('login', views.login, name='login'),
 ]
